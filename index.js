f621f316fd773a7e9683b45370598d78c9ff2c00
yaunghein <yaunghein@protonmail.com>
2021-01-21 18:44:57 +0630

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const introP = document.querySelector(".intro");
+const punchlineP = document.querySelector(".punchline");
+const punchlineBtn = document.querySelector(".punchlineBtn");
+const newJokeBtn = document.querySelector(".newJokeBtn");
+let punchline = "";
+
+const btnUpdate = () => {
+  punchlineBtn.classList.toggle("hide");
+  newJokeBtn.classList.toggle("hide");
+};
+
+const getJokes = async () => {
+  btnUpdate();
+  punchlineP.style.display = "none";
+  punchlineP.innerHTML = "";
+  const jokeRes = await fetch("https://official-joke-api.appspot.com/jokes/programming/random");
+  const jokeData = await jokeRes.json();
+  const joke = await jokeData[0].setup;
+  punchline = await jokeData[0].punchline;
+  introP.textContent = joke;
+};
+getJokes();
+
+const getPunchline = () => {
+  btnUpdate();
+  punchlineP.style.display = "block";
+  punchlineP.textContent = punchline;
+};
